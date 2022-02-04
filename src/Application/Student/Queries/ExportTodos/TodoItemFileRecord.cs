a9d9ec291a66118a87f1c2b6486e4b9edeec62c5
KronosSolucionesTIC <47668497+KronosSolucionesTIC@users.noreply.github.com>
2022-02-04 14:32:21 -0500

    Creacion de Student en Application

---
+++ src/Application/Student/Queries/ExportTodos/TodoItemFileRecord.cs
@@ -0,0 +1,11 @@
+﻿using CleanArchitecth.Application.Common.Mappings;
+using CleanArchitecth.Domain.Entities;
+
+namespace CleanArchitecth.Application.TodoLists.Queries.ExportTodos;
+
+public class TodoItemRecord : IMapFrom<TodoItem>
+{
+    public string? Title { get; set; }
+
+    public bool Done { get; set; }
+}
---
+++ src/Application/Student/Queries/GetTodos/TodoListDto.cs
@@ -0,0 +1,20 @@
+﻿using CleanArchitecth.Application.Common.Mappings;
+using CleanArchitecth.Domain.Entities;
+
+namespace CleanArchitecth.Application.TodoLists.Queries.GetTodos;
+
+public class TodoListDto : IMapFrom<TodoList>
+{
+    public TodoListDto()
+    {
+        Items = new List<TodoItemDto>();
+    }
+
+    public int Id { get; set; }
+
+    public string? Title { get; set; }
+
+    public string? Colour { get; set; }
+
+    public IList<TodoItemDto> Items { get; set; }
+}
