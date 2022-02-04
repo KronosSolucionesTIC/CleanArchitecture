467fa92ae820efc922de4468cd96a604add4a296
KronosSolucionesTIC <47668497+KronosSolucionesTIC@users.noreply.github.com>
2022-02-04 09:23:50 -0500

    Migracion de BD

---
+++ src/Infrastructure/Persistence/Migrations/20220204141743_SampleMigration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CleanArchitecth.Infrastructure.Persistence.Migrations
+{
+    public partial class SampleMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
